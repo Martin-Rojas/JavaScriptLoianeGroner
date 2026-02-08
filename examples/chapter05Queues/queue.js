248322a54bcca4cfc652113ac3b48c2477a90ffe
MartinRojas <rojas.martin25@yahoo.com>
2026-02-07 16:21:44 -0800

    basic structure of the queue class

---
+++ examples/chapter05Queues/queue.js
@@ -0,0 +1,19 @@
+import Node from "./node.js";
+
+class Queue {
+   constructor() {
+      this.front = null;
+      this.count = 0;
+      this.rear = null;
+   }
+   // Insert item from the back
+   enqueue(item) {}
+   // Remove item from the front
+   dequeue() {}
+   // get the front item
+   peek() {}
+   // Check if the queue it has items
+   isEmpty() {}
+   // Get the size of the Queue
+   size() {}
+}
