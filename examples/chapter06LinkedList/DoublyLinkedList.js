a74094aa9a55a7b4905dfe8199751036b7c680b9
MartinRojas <rojas.martin25@yahoo.com>
2026-02-13 20:31:25 -0800

    feat(DoublyLinkedList): basic method signatures

---
+++ examples/chapter06LinkedList/DoublyLinkedList.js
@@ -0,0 +1,34 @@
+import { LinkedList } from "./LinkedList.js";
+
+class DoublyLinkedList extends LinkedList {
+   constructor() {
+      super();
+      this.tail = null;
+   }
+
+   /* Insert a new element at the end (tail) of the list.*/
+   push(element) {}
+   /* Insert a new element at a specific index.*/
+   insert(element, position) {}
+   /* Remove a node at a specific index.*/
+   removeAt(position) {}
+   /* Remove first occurrence of an element.*/
+   remove(element) {}
+   /* Return the index of the first matching element.*/
+   indexOf(element) {}
+   /* Return the node at a specific index.*/
+   getElementAt(index) {}
+   /* Check if the list is empty.*/
+   isEmpty() {}
+
+   /* Return number of elements.*/
+   size() {}
+   /* Remove all elements.*/
+   clear() {}
+   /* Return string representation (forward traversal).*/
+   toString() {}
+   /** Return string representation (reverse traversal using prev). */
+   inverseToString() {}
+}
+
+export { DoublyLinkedList };
--- examples/chapter06LinkedList/main.js
+++ examples/chapter06LinkedList/main.js
@@ -1,55 +1,58 @@
 import { LinkedList } from "./LinkedList.js";
+import { DoublyLinkedList } from "./DoublyLinkedList.js";
 
-const list = new LinkedList();
-console.log(list);
+// const list = new LinkedList();
+// console.log(list);
 
-list.push(18);
-console.log(list);
+// list.push(18);
+// console.log(list);
 
-list.push(25);
-console.log(list);
+// list.push(25);
+// console.log(list);
 
-console.log("After the third node ");
+// console.log("After the third node ");
 
-list.push(38);
-console.log(list);
+// list.push(38);
+// console.log(list);
 
-console.log(`Size of the list ==> ${list.size()}`);
+// console.log(`Size of the list ==> ${list.size()}`);
 
-// console.log(list.insert(45, 2));
-// console.log(list.insert(45, 0));
-// console.log(list.insert(45, 1));
-// console.log(list.insert(45, 3));
-// console.log(list.insert(45, 2));
-//console.log(list.insert(89, 5));
-console.log(list.toString());
+// // console.log(list.insert(45, 2));
+// // console.log(list.insert(45, 0));
+// // console.log(list.insert(45, 1));
+// // console.log(list.insert(45, 3));
+// // console.log(list.insert(45, 2));
+// //console.log(list.insert(89, 5));
+// console.log(list.toString());
 
-console.log(list.insert(89, 3));
-console.log(list.insert(3, 4));
-console.log(list.insert(31, 5));
+// console.log(list.insert(89, 3));
+// console.log(list.insert(3, 4));
+// console.log(list.insert(31, 5));
 
-console.log(`after the insertions`);
-console.log(list.toString());
+// console.log(`after the insertions`);
+// console.log(list.toString());
 
-console.log(list.getElementAt(2));
-console.log(list.getElementAt(8));
-console.log(list.getElementAt(0));
-console.log(list.getElementAt(4));
-console.log(list.getElementAt(5));
+// console.log(list.getElementAt(2));
+// console.log(list.getElementAt(8));
+// console.log(list.getElementAt(0));
+// console.log(list.getElementAt(4));
+// console.log(list.getElementAt(5));
 
-console.log(`Test remove method`);
-console.log(list.remove(18));
-console.log(list.remove(38));
-console.log(list.remove(3));
-console.log(list.remove(31));
-console.log(list.remove(380));
-console.log(list.toString());
+// console.log(`Test remove method`);
+// console.log(list.remove(18));
+// console.log(list.remove(38));
+// console.log(list.remove(3));
+// console.log(list.remove(31));
+// console.log(list.remove(380));
+// console.log(list.toString());
 
-console.log(`Test indexOf method`);
-list.push(98);
-list.push(18);
-list.push(77);
-list.push(7);
-console.log(list.indexOf(25));
-console.log(list.indexOf(77));
-console.log(list.toString());
+// console.log(`Test indexOf method`);
+// list.push(98);
+// list.push(18);
+// list.push(77);
+// list.push(7);
+// console.log(list.indexOf(25));
+// console.log(list.indexOf(77));
+// console.log(list.toString());
+const doublyList = new DoublyLinkedList();
+console.log(doublyList);
